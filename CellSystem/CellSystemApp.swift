e6462b9cdc100e48e0dc365aee443f8da12fbcc8
krazy7 <98artem@gmail.com>
2026-07-31 15:55:01 +0200

    Initial Commit

---
+++ CellSystem/CellSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellSystemApp.swift
+//  CellSystem
+//
+//  Created by Artsem Hotsau on 31/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CellSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
